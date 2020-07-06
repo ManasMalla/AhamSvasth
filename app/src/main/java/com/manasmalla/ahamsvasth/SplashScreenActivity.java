3f6f85b4d3a157996dbdbaa19376fa776749ff86
Lakshit Malla <techecosafari@gmail.com>
2020-07-06 20:19:40 +0530

    SplashScreenActivity

--- app/src/main/java/com/manasmalla/ahamsvasth/SplashScreenActivity.java
+++ app/src/main/java/com/manasmalla/ahamsvasth/SplashScreenActivity.java
@@ -2,7 +2,11 @@ package com.manasmalla.ahamsvasth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
     }
-}
+     public  void ahamSvasth(View view){
+         ImageView ahamSvasth = (ImageView) findViewById(R.id.imageView) ;
+         ahamSvasth.animate().setDuration(2000);
+         Handler handler = new Handler();
+         Runnable runnable = new Runnable() {
+             @Override
+             public void run() {
+                 Intent intent = new Intent(getApplicationContext(),  LoginActivity.class);
+                 startActivity(intent);
+             }
+         };
+
+         handler.postDelayed(runnable, 2000);
+
+     }
+
+
+
+    }
